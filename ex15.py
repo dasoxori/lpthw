24ac43b1ed1910017919e34e316a51a267822cf1
dimopoulosvtec@yahoo.gr <dimopoulosvtec@yahoo.gr>
2020-03-29 18:36:17 +0300

    Reading Files

---
+++ ex15.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the filename again:"
+fileAgain = raw_input("> ")
+
+txtAgain = open(fileAgain)
+
+print txtAgain.read()
