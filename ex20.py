9b0fcde968dc85653787a0b21bd279a1611bf785
dimopoulosvtec@yahoo.gr <dimopoulosvtec@yahoo.gr>
2020-04-03 16:51:30 +0300

    More Functions and Variables with files

---
+++ ex20.py
@@ -0,0 +1,33 @@
+from sys import argv
+
+script, inputFile = argv
+
+def printAll(f):
+    print f.read()
+
+def rewind(f):
+    f.seek(0)
+
+def printALine(lineCount, f):
+    print lineCount, f.readline()
+
+currentFile = open(inputFile)
+
+print "First let's print the whole file:\n"
+
+printAll(currentFile)
+
+print "Now let's rewind, kind like a tape."
+
+rewind(currentFile)
+
+print "Let's print three lines:"
+
+currentLine = 1
+printALine(currentLine, currentFile)
+
+currentLine = 2
+printALine(currentLine, currentFile)
+
+currentLine = 3
+printALine(currentLine, currentFile)
